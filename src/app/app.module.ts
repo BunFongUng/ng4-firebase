e973e9b0c96d1d375db4bfd0012afc65106237a4
Lester <lester.ung@pathmazing.com>
2017-08-21 11:23:27 +0700

    connect to firebase and define 3 methods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,23 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+/**
+ * imports angularFire2 and firebae modules
+ */
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 import { AppComponent } from './app.component';
+
+export const firebaseConfig: any = {
+  apiKey: 'AIzaSyA4dlPcWBIUMQYdjJ8cr8sVBG4MfWwhh2s',
+  authDomain: 'ng4-firebase-54aa5.firebaseapp.com',
+  databaseURL: 'https://ng4-firebase-54aa5.firebaseio.com',
+  projectId: 'ng4-firebase-54aa5',
+  storageBucket: '',
+  messagingSenderId: '80291671676'
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +28,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
